ae24daeb038ae48fc536a6dd4030328ae9e2223a
Flvrset <mkominiak11@gmail.com>
2022-06-30 20:55:26 +0200

    Added Put, Delete, Post and Get controllers with an office service

--- src/main/java/com/example/spring_database/SpringDatabaseApplication.java
+++ src/main/java/com/example/spring_database/SpringDatabaseApplication.java
@@ -7,26 +7,14 @@ import com.example.spring_database.repository.OfficesRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Arrays;
 import java.util.List;
 
 @SpringBootApplication
 public class SpringDatabaseApplication {
-
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(SpringDatabaseApplication.class, args);
-        OfficesRepository officesRepository = context.getBean(OfficesRepository.class);
-        EmployeesRepository employeesRepository = context.getBean(EmployeesRepository.class);
-
-        Offices biuro1 = new Offices("Bielsko", "123456789", "3 Maja", "Poland", "43-385");
-        Employees marek = new Employees("Kowalski", "Marek", "maromaro@gmail.com");
-        marek.setOffice(biuro1);
-        Employees basia = new Employees("Szota", "Basia", "email@email.com");
-        basia.setOffice(biuro1);
-        officesRepository.save(biuro1);
-        employeesRepository.saveAll(Arrays.asList(marek, basia));
-
-
+        SpringApplication.run(SpringDatabaseApplication.class, args);
     }
 }
