1505bea9e5e380814b096452cdf9c4f8e296bdd1
Flvrset <mkominiak11@gmail.com>
2022-07-08 14:21:38 +0200

    Added Put, Delete, Post and Get controllers with an office service

--- src/main/java/com/example/spring_database/SpringDatabaseApplication.java
+++ src/main/java/com/example/spring_database/SpringDatabaseApplication.java
@@ -1,16 +1,7 @@
 package com.example.spring_database;
 
-import com.example.spring_database.model.Employees;
-import com.example.spring_database.model.Offices;
-import com.example.spring_database.repository.EmployeesRepository;
-import com.example.spring_database.repository.OfficesRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.Arrays;
-import java.util.List;
 
 @SpringBootApplication
 public class SpringDatabaseApplication {
